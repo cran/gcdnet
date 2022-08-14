4e14d2e32651deff9ee005e3230cfc53b7e2fef1
Yi Yang <yi.yang6@mcgill.ca>
2022-08-14 01:30:02 +0000

    version 1.0.6

--- R/predict.gcdnet.R
+++ R/predict.gcdnet.R
@@ -1,2 +1,76 @@
-predict.gcdnet <- function(object, newx, s = NULL, 
-    type = c("class", "link"), ...) NextMethod("predict") 
+##' Make predictions from a "gcdnet" object
+##'
+##' Similar to other predict methods, this functions predicts fitted values and
+##' class labels from a fitted \code{\link{gcdnet}} object.
+##'
+##' \code{s} is the new vector at which predictions are requested. If \code{s}
+##' is not in the lambda sequence used for fitting the model, the
+##' \code{predict} function will use linear interpolation to make predictions.
+##' The new values are interpolated using a fraction of predicted values from
+##' both left and right \code{lambda} indices.
+##'
+##' @aliases predict.gcdnet predict.hsvmpath predict.sqsvmpath predict.logitpath
+##'   predict.lspath predict.erpath
+##'
+##' @param object fitted \code{\link{gcdnet}} model object.
+##' @param newx matrix of new values for \code{x} at which predictions are to be
+##'   made. NOTE: \code{newx} must be a matrix, \code{predict} function does not
+##'   accept a vector or other formats of \code{newx}.
+##' @param s value(s) of the penalty parameter \code{lambda} at which
+##'   predictions are required. Default is the entire sequence used to create
+##'   the model.
+##' @param type type of prediction required. \itemize{ \item Type \code{"link"}
+##'   gives the linear predictors for classification problems and gives
+##'   predicted response for regression problems. \item Type \code{"class"}
+##'   produces the class label corresponding to the maximum probability. Only
+##'   available for classification problems.}
+##' @param \dots Not used. Other arguments to predict.
+##' @return The object returned depends on type.
+##' @author Yi Yang, Yuwen Gu and Hui Zou\cr
+##'
+##' Maintainer: Yi Yang <yi.yang6@mcgill.ca>
+##' @seealso \code{\link{coef}} method
+##' @references Yang, Y. and Zou, H. (2012).
+##'   "An Efficient Algorithm for Computing The HHSVM and Its Generalizations."
+##'   \emph{Journal of Computational and Graphical Statistics}, 22, 396-415.\cr
+##'   BugReport: \url{https://github.com/emeryyi/gcdnet}\cr
+##'
+##'   Gu, Y., and Zou, H. (2016).
+##'   "High-dimensional generalizations of asymmetric least squares regression and their applications."
+##'   \emph{The Annals of Statistics}, 44(6), 2661–2694.\cr
+##'
+##'   Friedman, J., Hastie, T., and Tibshirani, R. (2010).
+##'   "Regularization paths for generalized linear models via coordinate descent."
+##'   \emph{Journal of Statistical Software, 33, 1.}\cr
+##'   \url{https://www.jstatsoft.org/v33/i01/}
+##' @keywords models regression
+##' @examples
+##'
+##' data(FHT)
+##' m1 <- gcdnet(x = FHT$x,y = FHT$y)
+##' print(predict(m1, type = "class",newx = FHT$x[2:5, ]))
+##'
+##' @export
+predict.gcdnet <- function(object, newx, s = NULL, type = c("class", "link"),
+                           ...) NextMethod("predict")
+
+##' Model predictions
+##'
+##' \code{predict} is a generic function for predictions from the results of
+##' various model fitting functions. The function invokes particular
+##' \emph{methods} which depend on the \code{\link{class}} of the first
+##' argument.
+##'
+##' @param object a model object for which prediction is desired.
+##' @param \dots additional arguments affecting the predictions produced.
+##'
+##' @return The form of the value returned by \code{predict} depends on the
+##'   class of its argument. See the documentation of the particular methods for
+##'   details of what is produced by that method.
+##'
+##' @seealso \code{\link{predict.gcdnet}}, \code{\link{predict.erpath}},
+##'   \code{\link{predict.lspath}}, \code{\link{predict.hsvmpath}},
+##'   \code{\link{predict.logitpath}}, \code{\link{predict.sqsvmpath}}.
+##'
+##' @export
+predict <- function(object,...) UseMethod("predict")
--- src/auxiliary.f90
+++ src/auxiliary.f90
@@ -1,33 +1,35 @@
-! DESCRIPTION: 
+! DESCRIPTION:
 !
-!    These functions are minor modifications from the glmnet package:
+!    These functions are minor modifications from the `glmnet` package:
 !
-!    Jerome Friedman, Trevor Hastie, Robert Tibshirani (2010). 
-!    Regularization Paths for Generalized Linear Models via Coordinate Descent. 
-!    Journal of Statistical Software, 33(1), 1-22. 
-!    URL http://www.jstatsoft.org/v33/i01/.
+!    Jerome Friedman, Trevor Hastie, Robert Tibshirani (2010).
+!    Regularization Paths for Generalized Linear Models via Coordinate Descent.
+!    Journal of Statistical Software, 33(1), 1-22.
+!    URL: https://www.jstatsoft.org/v33/i01/.
 !
 ! --------------------------------------------------------------------------
 ! standard: An auxiliary function for standardize x matrix.
 ! --------------------------------------------------------------------------
 !
 ! USAGE:
-! 
-! call standard (nobs,nvars,x,ju,isd,xmean,xnorm,maj)   
-! 
+!
+! CALL standard(nobs, nvars, x, ju, isd, intr, xmean, xnorm, maj)
+!
 ! INPUT ARGUMENTS:
-! 
+!
 !    nobs = number of observations
 !    nvars = number of predictor variables
-!    x(nobs, nvars) = matrix of predictors, of dimension N * p; each row is an observation vector.
+!    x(nobs, nvars) = matrix of predictors, of dimension N * p; each row is an
+!                     observation vector.
 !    ju(nvars) = flag of predictor variables
 !                ju(j) = 0 => this predictor has zero variance
 !                ju(j) = 1 => this predictor does not have zero variance
 !    isd = standarization flag:
 !          isd = 0 => do not standardize predictor variables
 !          isd = 1 => standardize predictor variables
-!          NOTE: no matter isd is 1 or 0, matrix x is always centered by column. That is, col.mean(x) = 0.
-!    
+!          NOTE: no matter isd is 1 or 0, matrix x is always centered by column.
+!                That is, col.mean(x) = 0.
+!
 ! OUTPUT:
 !
 !    x(nobs, nvars) = standarized matrix x
@@ -40,17 +42,18 @@
 ! --------------------------------------------------------------------------
 !
 ! USAGE:
-! 
-! call chkvars (nobs, nvars, x, ju)
-! 
+!
+! CALL chkvars(nobs, nvars, x, ju)
+!
 ! INPUT ARGUMENTS:
-! 
+!
 !    nobs = number of observations
 !    nvars = number of predictor variables
-!    x(nobs, nvars) = matrix of predictors, of dimension N * p; each row is an observation vector.
-!    y(nobs) = response variable. This argument should be a two-level factor {-1, 1} 
-!            for classification.
-!    
+!    x(nobs, nvars) = matrix of predictors, of dimension N * p; each row is an
+!                     observation vector.
+!    y(nobs) = response variable. This argument should be a two-level factor
+!              {-1, 1} for classification.
+!
 ! OUTPUT:
 !
 !    ju(nvars) = flag of predictor variables
@@ -58,59 +61,78 @@
 !                ju(j) = 1 => this predictor does not have zero variance
 !
 
-! --------------------------------------------------
-SUBROUTINE standard(nobs,nvars,x,ju,isd,xmean,xnorm,maj)     
-! --------------------------------------------------
-    IMPLICIT NONE
-    ! - - - arg types - - -
-    INTEGER::nobs
-    INTEGER::nvars
-    INTEGER::isd
-    INTEGER::ju(nvars)
-    DOUBLE PRECISION::x(nobs,nvars)
-    DOUBLE PRECISION::xmean(nvars)
-    DOUBLE PRECISION::xnorm(nvars)
-    DOUBLE PRECISION::maj(nvars)
-    ! - - - local declarations - - -
-    INTEGER:: j
-! - - - begin - - -                                
-    DO j=1,nvars                                  
-        IF(ju(j)==1) THEN                         
-            xmean(j)=sum(x(:,j))/nobs     !mean                        
-            x(:,j)=x(:,j)-xmean(j)    
-            maj(j)=dot_product(x(:,j),x(:,j))/nobs                                              
-              IF(isd==1) THEN
-                xnorm(j)=sqrt(maj(j))    !standard deviation               
-                x(:,j)=x(:,j)/xnorm(j)
-                maj(j)=1.0D0
-            ENDIF                                                        
-        ENDIF                                     
-    ENDDO                             
+! --------------------------------------------------------------------------
+SUBROUTINE standard(nobs, nvars, x, ju, isd, intr, xmean, xnorm, maj)
+  ! ------------------------------------------------------------------------
+  IMPLICIT NONE
+  ! -------- INPUT VARIABLES -------- !
+  INTEGER :: nobs
+  INTEGER :: nvars
+  INTEGER :: isd
+  INTEGER :: intr
+  INTEGER :: ju(nvars)
+  DOUBLE PRECISION :: xmsq
+  DOUBLE PRECISION :: xvar
+  DOUBLE PRECISION :: x(nobs, nvars)
+  DOUBLE PRECISION :: xmean(nvars)
+  DOUBLE PRECISION :: xnorm(nvars)
+  DOUBLE PRECISION :: maj(nvars)
+  ! -------- LOCAL DECLARATIONS -------- !
+  INTEGER :: j
+  ! -------- STANDARDIZATION -------- !
+  IF (intr == 0) THEN
+     DO j = 1, nvars
+        IF (ju(j) == 1) THEN
+           xmean(j) = 0.0D0
+           maj(j) = DOT_PRODUCT(x(:,j), x(:,j)) / nobs
+           IF (isd == 1) THEN
+              xmsq = (SUM(x(:,j)) / nobs)**2
+              xvar = maj(j) - xmsq
+              xnorm(j) = SQRT(xvar)
+              x(:,j) = x(:,j) / xnorm(j)
+              maj(j) = 1.0D0 + xmsq / xvar
+           END IF
+        END IF
+     END DO
+  ELSE
+     DO j = 1, nvars
+        IF (ju(j) == 1) THEN
+           xmean(j) = SUM(x(:,j)) / nobs ! MEAN
+           x(:,j) = x(:,j) - xmean(j)
+           maj(j) = DOT_PRODUCT(x(:,j), x(:,j)) / nobs
+           IF (isd == 1) THEN
+              xnorm(j) = SQRT(maj(j)) ! STANDARD DEVIATION
+              x(:,j) = x(:,j) / xnorm(j)
+              maj(j) = 1.0D0
+           END IF
+        END IF
+     END DO
+  END IF
 END SUBROUTINE standard
 
 
-! --------------------------------------------------
-SUBROUTINE chkvars (nobs, nvars, x, ju)
-! --------------------------------------------------
-      IMPLICIT NONE
-    ! - - - arg types - - -
-      INTEGER :: nobs
-      INTEGER :: nvars
-      INTEGER :: ju (nvars)
-      DOUBLE PRECISION :: x (nobs, nvars)
-    ! - - - local declarations - - -
-      INTEGER :: i
-      INTEGER :: j
-      DOUBLE PRECISION :: t
-! - - - begin - - -
-      DO j = 1, nvars
-         ju (j) = 0
-         t = x (1, j)
-         DO i = 2, nobs
-            IF (x(i, j) /= t) THEN
-               ju (j) = 1
-               EXIT
-            END IF
-         END DO
-      END DO
+! -------------------------------------------------------------------------
+SUBROUTINE chkvars(nobs, nvars, x, ju)
+  ! -----------------------------------------------------------------------
+  IMPLICIT NONE
+  ! -------- INPUT VARIABLES -------- !
+  INTEGER :: nobs
+  INTEGER :: nvars
+  INTEGER :: ju(nvars)
+  DOUBLE PRECISION :: x(nobs, nvars)
+  ! -------- LOCAL DECLARATIONS -------- !
+  INTEGER :: i
+  INTEGER :: j
+  DOUBLE PRECISION :: t
+  ! -------- VARIABLE CHECKING -------- !
+  DO j = 1, nvars
+     ju(j) = 0
+     t = x(1, j)
+     DO i = 2, nobs
+        IF (x(i, j) /= t) THEN
+           ju(j) = 1
+           EXIT
+        END IF
+     END DO
+  END DO
 END SUBROUTINE chkvars
